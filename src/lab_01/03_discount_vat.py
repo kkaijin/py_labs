caeaca8d8d195beb6fd811a413bbc4bffca7090f
Денис Капитонов <deniskapitonov@MacBook-Pro-Denis.local>
2025-09-16 16:27:52 +0300

    readme

--- src/lab_01/03_discount_vat.py
+++ src/lab_01/03_discount_vat.py
@@ -1,11 +1,14 @@
-price = float(input())
-discount = float(input())
-vat = float(input())
+price = input('price=')
+discount = input('discount=')
+vat = input('vat=')
+price = float(price)
+discount = float(discount)
+vat = float(vat)
 
 base = price * (1 - discount/100)
 vat_amount = base*(vat/100)
 total = base+vat_amount
 
-print('База посл скидки:', round(base,2))
-print('НДС:', round(vat_amount,2))
-print('Итого к оплате:', round(total, 2))
+print('База посл скидки:', "{:.2f}".format(round(base,2)))
+print('НДС:', "{:.2f}".format(round(vat_amount,2)))
+print('Итого к оплате:', "{:.2f}".format(round(total, 2)))
